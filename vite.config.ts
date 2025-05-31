64f67b638b9d8969b735f5afdcd9fd3061721a74
pauldolsen <pauldolsen@verizon.net>
2025-05-31 09:49:06 -0400

    tailwind 3 remove

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
-import tailwind from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -10,11 +8,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [VueRouter(), vue(), vueDevTools()],
-  css: {
-    postcss: {
-      plugins: [tailwind(), autoprefixer()],
-    },
-  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
